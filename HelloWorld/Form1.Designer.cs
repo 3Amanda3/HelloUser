77db6650f957c6e7799013d4fd17ae7eb8fc05f9
Amanda Schepp <mandybear1@icloud.com>
2016-10-25 12:21:14 -0500

    hello user changes
    added more things

--- HelloWorld/Form1.Designer.cs
+++ HelloWorld/Form1.Designer.cs
@@ -29,36 +29,151 @@
         private void InitializeComponent()
         {
             this.lblResult = new System.Windows.Forms.Label();
-            this.btReveal = new System.Windows.Forms.Button();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.rdbEnglish = new System.Windows.Forms.RadioButton();
+            this.rdbFrench = new System.Windows.Forms.RadioButton();
+            this.rdbSpanish = new System.Windows.Forms.RadioButton();
+            this.grbLanguage = new System.Windows.Forms.GroupBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblDisplayDays = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.btnAgeDisplay = new System.Windows.Forms.Button();
+            this.grbLanguage.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblResult
             // 
-            this.lblResult.AutoSize = true;
-            this.lblResult.Location = new System.Drawing.Point(12, 91);
+            this.lblResult.Location = new System.Drawing.Point(12, 283);
             this.lblResult.Name = "lblResult";
-            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.Size = new System.Drawing.Size(345, 196);
             this.lblResult.TabIndex = 2;
+            this.lblResult.Click += new System.EventHandler(this.lblResult_Click);
             // 
-            // btReveal
+            // lblQuestion
             // 
-            this.btReveal.Location = new System.Drawing.Point(13, 13);
-            this.btReveal.Name = "btReveal";
-            this.btReveal.Size = new System.Drawing.Size(129, 28);
-            this.btReveal.TabIndex = 3;
-            this.btReveal.Text = "Click to reveal";
-            this.btReveal.UseVisualStyleBackColor = true;
-            this.btReveal.Click += new System.EventHandler(this.btReveal_Click);
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Location = new System.Drawing.Point(29, 116);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(153, 13);
+            this.lblQuestion.TabIndex = 4;
+            this.lblQuestion.Text = "What language do you speak?";
+            // 
+            // rdbEnglish
+            // 
+            this.rdbEnglish.AutoSize = true;
+            this.rdbEnglish.Location = new System.Drawing.Point(35, 19);
+            this.rdbEnglish.Name = "rdbEnglish";
+            this.rdbEnglish.Size = new System.Drawing.Size(59, 17);
+            this.rdbEnglish.TabIndex = 5;
+            this.rdbEnglish.TabStop = true;
+            this.rdbEnglish.Text = "English";
+            this.rdbEnglish.UseVisualStyleBackColor = true;
+            this.rdbEnglish.CheckedChanged += new System.EventHandler(this.rdbEnglish_CheckedChanged);
+            // 
+            // rdbFrench
+            // 
+            this.rdbFrench.AutoSize = true;
+            this.rdbFrench.Location = new System.Drawing.Point(35, 42);
+            this.rdbFrench.Name = "rdbFrench";
+            this.rdbFrench.Size = new System.Drawing.Size(58, 17);
+            this.rdbFrench.TabIndex = 6;
+            this.rdbFrench.TabStop = true;
+            this.rdbFrench.Text = "French";
+            this.rdbFrench.UseVisualStyleBackColor = true;
+            this.rdbFrench.CheckedChanged += new System.EventHandler(this.rdbFrench_CheckedChanged);
+            // 
+            // rdbSpanish
+            // 
+            this.rdbSpanish.AutoSize = true;
+            this.rdbSpanish.Location = new System.Drawing.Point(35, 65);
+            this.rdbSpanish.Name = "rdbSpanish";
+            this.rdbSpanish.Size = new System.Drawing.Size(63, 17);
+            this.rdbSpanish.TabIndex = 7;
+            this.rdbSpanish.TabStop = true;
+            this.rdbSpanish.Text = "Spanish";
+            this.rdbSpanish.UseVisualStyleBackColor = true;
+            this.rdbSpanish.CheckedChanged += new System.EventHandler(this.rdbSpanish_CheckedChanged);
+            // 
+            // grbLanguage
+            // 
+            this.grbLanguage.Controls.Add(this.rdbEnglish);
+            this.grbLanguage.Controls.Add(this.rdbSpanish);
+            this.grbLanguage.Controls.Add(this.rdbFrench);
+            this.grbLanguage.Location = new System.Drawing.Point(32, 152);
+            this.grbLanguage.Name = "grbLanguage";
+            this.grbLanguage.Size = new System.Drawing.Size(200, 100);
+            this.grbLanguage.TabIndex = 8;
+            this.grbLanguage.TabStop = false;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(29, 29);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(101, 13);
+            this.lblName.TabIndex = 9;
+            this.lblName.Text = "What is your name?";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(67, 63);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 10;
+            // 
+            // lblDisplayDays
+            // 
+            this.lblDisplayDays.Location = new System.Drawing.Point(346, 152);
+            this.lblDisplayDays.Name = "lblDisplayDays";
+            this.lblDisplayDays.Size = new System.Drawing.Size(229, 128);
+            this.lblDisplayDays.TabIndex = 16;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(376, 63);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 20);
+            this.txtAge.TabIndex = 15;
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(373, 29);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(110, 13);
+            this.lblAge.TabIndex = 14;
+            this.lblAge.Text = "Please enter your age";
+            // 
+            // btnAgeDisplay
+            // 
+            this.btnAgeDisplay.Location = new System.Drawing.Point(376, 105);
+            this.btnAgeDisplay.Name = "btnAgeDisplay";
+            this.btnAgeDisplay.Size = new System.Drawing.Size(107, 24);
+            this.btnAgeDisplay.TabIndex = 17;
+            this.btnAgeDisplay.Text = "Click to find out!";
+            this.btnAgeDisplay.UseVisualStyleBackColor = true;
+            this.btnAgeDisplay.Click += new System.EventHandler(this.btnAgeDisplay_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.btReveal);
+            this.ClientSize = new System.Drawing.Size(749, 622);
+            this.Controls.Add(this.btnAgeDisplay);
+            this.Controls.Add(this.lblDisplayDays);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.grbLanguage);
+            this.Controls.Add(this.lblQuestion);
             this.Controls.Add(this.lblResult);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.grbLanguage.ResumeLayout(false);
+            this.grbLanguage.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,7 +181,17 @@
 
         #endregion
         private System.Windows.Forms.Label lblResult;
-        private System.Windows.Forms.Button btReveal;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.RadioButton rdbEnglish;
+        private System.Windows.Forms.RadioButton rdbFrench;
+        private System.Windows.Forms.RadioButton rdbSpanish;
+        private System.Windows.Forms.GroupBox grbLanguage;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblDisplayDays;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Button btnAgeDisplay;
     }
 }
 
--- HelloWorld/Form1.cs
+++ HelloWorld/Form1.cs
@@ -8,6 +8,22 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/**
+
+ *
+
+ * Amanda Schepp 
+
+ * Mr.Hardman 
+
+ * Lab 2, Hello user
+
+ *October 13 
+
+ *
+ */
+
+
 namespace HelloWorld
 {
     public partial class Form1 : Form
@@ -29,7 +45,40 @@ namespace HelloWorld
 
         private void btReveal_Click(object sender, EventArgs e)
         {
-            lblResult.Text = "Hello, Human!";
+            lblResult.Text = "You are " + txtName.Text;
+        }
+
+        private void rdbEnglish_CheckedChanged(object sender, EventArgs e)
+        {
+            lblResult.Text = "Hello! "+txtName.Text;
+        }
+
+        private void rdbFrench_CheckedChanged(object sender, EventArgs e)
+        {
+            lblResult.Text = "Salut! " +txtName.Text;
+        }
+
+        private void rdbSpanish_CheckedChanged(object sender, EventArgs e)
+        {
+            lblResult.Text = "Hola! " +txtName.Text;
+        }
+
+        private void lblResult_Click(object sender, EventArgs e)
+        {
+           
+         
+
+
+        }
+
+        private void btnAgeDisplay_Click(object sender, EventArgs e)
+        {
+            double Age = Convert.ToDouble(txtAge.Text);
+            double Year = 365.25;
+
+            double Days = (Age*Year);
+            lblDisplayDays.Text = "You were " + Days + " days old on your" +Age+ "th birthday!";
+
         }
     }
 }
